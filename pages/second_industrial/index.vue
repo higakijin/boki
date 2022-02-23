60bb6eaf48fce30bc471cf357deb603712216f33
higakijin <hm385.chejptks@gmail.com>
2022-02-23 15:52:03 +0900

    リンクをつけた

--- pages/second_industrial/index.vue
+++ pages/second_industrial/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex w-screen h-screen text-gray-700">
+  <div class="flex w-screen h-screen text-gray-700 overflow-hidden">
     <Icons />
     <div class="flex flex-col flex-grow h-full">
       <Header />
-      <section class="grid grid-cols-12 mt-10">
+      <section class="grid grid-cols-12 my-10 overflow-scroll">
         <h1 class="col-span-5 col-start-4 text-3xl py-10 font-bold">2級工業簿記</h1>
-        <div v-for="chapter in $secondIndustrialChapters" :key="chapter.id" class="col-span-5 col-start-4 flex rounded-xl shadow border border-gray-300 mb-4">
+        <NuxtLink v-for="chapter in $secondIndustrialChapters" :key="chapter.id" :to="`/${$route.name}/chapter/${chapter.id}/lesson/1`" class="col-span-5 col-start-4 flex rounded-xl shadow border border-gray-300 mb-4">
           <div class="h-10 w-10 rounded-full bg-blue-500 m-2 flex">
             <p class="text-white mx-auto my-auto text-lg font-bold">2工</p>
           </div>
           <p class="text-xl my-auto px-4">Chapter.{{ chapter.id }}　{{ chapter.title }}</p>
-        </div>
+        </NuxtLink>
       </section>
     </div>
   </div>
