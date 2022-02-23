73d96131eab1d18ca302d88826d769b074898ad6
higakijin <hm385.chejptks@gmail.com>
2022-02-23 16:38:58 +0900

    ドロップダウンを編集

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -2,7 +2,10 @@
   <div class="flex flex-col w-56 border-r border-gray-300">
     <button class="relative text-sm focus:outline-none group">
       <div class="flex items-center justify-between w-full h-16 px-4 border-b border-gray-300 hover:bg-gray-300">
-        <span class="font-medium">工業簿記とは</span>
+        <div>
+          <p class="font-medium text-left">chapter.{{ $route.params.chapterId }}</p>
+          <p class="font-medium text-left">{{ title }}</p>
+        </div>
         <SvgInverseTriangle />
       </div>
       <div class="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white shadow-lg group-focus:flex">
@@ -12,8 +15,8 @@
       </div>
     </button>
     <div class="flex flex-col flex-grow p-4 overflow-auto">
-      <a v-for="chapter in chapters" :key="chapter.id" class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300" href="#">
-        <span class="leading-none">{{ chapter.id }}. {{ chapter.title }}</span>
+      <a v-for="lesson in lessons" :key="lesson.id" class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300" href="#">
+        <span class="leading-none">{{ lesson.id }}. {{ lesson.title }}</span>
       </a>
       <a class="flex items-center flex-shrink-0 h-10 px-3 mt-auto text-sm font-medium bg-gray-200 rounded hover:bg-gray-300" href="#">
         <SvgBack />
@@ -27,12 +30,8 @@
 export default {
   data() {
     return {
-      chapters: [
-        { id: 1, title: '工業簿記とは' },
-        { id: 2, title: '工業簿記の全体の流れ' },
-        { id: 3, title: '原価計算とは' },
-        { id: 4, title: '仕掛品と製品' },
-      ],
+      title:  this.$secondIndustrialChapters[this.$route.params.chapterId - 1].title,
+      lessons: this.$secondIndustrialChapters[this.$route.params.chapterId - 1].lessons
     }
   },
 }
