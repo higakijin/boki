306b782e95768c443c444006011a98a9a2d7f862
higakijin <hm385.chejptks@gmail.com>
2022-02-27 18:41:53 +0900

    adminの追加

---
+++ pages/admin/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen overflow-hidden">
+    <!-- Section 1 -->
+    <TopNav />
+
+    <section class="flex flex-col md:flex-row h-full items-center">
+      <div class="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-full px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+        <div class="w-full h-100">
+          <h1 class="text-xl md:text-2xl font-bold leading-tight">Create super future</h1>
+
+          <form class="mt-6" action="#" method="POST">
+            <div>
+              <label class="block text-gray-700">Email Address</label>
+              <input type="email" name="" id="" placeholder="" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required />
+            </div>
+
+            <div class="mt-4">
+              <label class="block text-gray-700">Password</label>
+              <input type="password" name="" id="" placeholder="" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required />
+            </div>
+
+            <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">Log In</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ pages/admin/users/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <TopNav />
+
+    <div class="text-gray-900 bg-gray-200">
+      <div class="p-4 flex">
+        <h1 class="text-3xl">Users</h1>
+      </div>
+      <div class="px-3 py-4 flex justify-center">
+        <table class="w-full text-md bg-white shadow-md rounded mb-4">
+          <tbody>
+            <tr class="border-b">
+              <th class="text-left p-3 px-5">アイコン</th>
+              <th class="text-left p-3 px-5">Name</th>
+              <th class="text-left p-3 px-5">Email</th>
+              <th class="text-left p-3 px-5">学習中のレベル</th>
+              <th class="text-left p-3 px-5">会員ステータス</th>
+              <th class="text-left p-3 px-5">最終支払日</th>
+              <th class="text-left p-3 px-5">有効期限</th>
+              <th class="text-left p-3 px-5">最終ログイン</th>
+              <th class="text-left p-3 px-5">詳細</th>
+            </tr>
+            <tr class="border-b bg-gray-100">
+              <td class="px-5"><img class="h-8" src="https://www.asahi-net.or.jp/~yq3t-hruc/img/USL.GIF" /></td>
+              <td class="p-3 px-5">higakijin</td>
+              <td class="p-3 px-5">hm385.chejptks@gmail.com</td>
+
+              <td class="p-3 px-5">2級工業</td>
+              <td class="p-3 px-5">プロ</td>
+              <td class="p-3 px-5">2021.12.24</td>
+              <td class="p-3 px-5">永久ライセンス</td>
+              <td class="p-3 px-5">ログイン中</td>
+              <td class="p-3 px-5 flex">
+                <button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Detail</button>
+              </td>
+            </tr>
+            <tr class="border-b">
+              <td class="px-5"><img class="h-8" src="https://m.media-amazon.com/images/I/31XDaI1437L._AC_SX355_.jpg" /></td>
+              <td class="p-3 px-5">野生のミシシッピズワイガニ</td>
+              <td class="p-3 px-5">waild.zuwai@kanikani.com</td>
+              <td class="p-3 px-5">3級</td>
+              <td class="p-3 px-5">プラス</td>
+              <td class="p-3 px-5">2022.2.26</td>
+              <td class="p-3 px-5">2022.3.26</td>
+              <td class="p-3 px-5">3ヶ月前</td>
+              <td class="p-3 px-5 flex">
+                <button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Detail</button>
+              </td>
+            </tr>
+            <tr class="border-b bg-gray-100">
+              <td class="px-5"><img class="h-8" src="https://www.asahi-net.or.jp/~yq3t-hruc/img/USL.GIF" /></td>
+              <td class="p-3 px-5">hogehoge</td>
+              <td class="p-3 px-5">hogehoge@foobar.com</td>
+
+              <td class="p-3 px-5">2級商業</td>
+              <td class="p-3 px-5">ベーシック</td>
+              <td class="p-3 px-5">なし</td>
+              <td class="p-3 px-5">なし</td>
+              <td class="p-3 px-5">1日前</td>
+              <td class="p-3 px-5 flex">
+                <button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Detail</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
