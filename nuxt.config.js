c0677da2d7dbf3c40e4e4f4035e5972abf9b3d85
higakijin <hm385.chejptks@gmail.com>
2022-03-04 13:35:32 +0900

    baseurlの変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5050', //プロダクション用の記述も後で追加
+    baseURL: process.env.NODE_ENV === "production" ? "https://bokunoboki-backend.herokuapp.com" : "http://localhost:5050",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
