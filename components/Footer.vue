8f44ec588441b1a8d9e1775fa907dca0129b2ffa
higakijin <hm385.chejptks@gmail.com>
2022-03-12 21:22:01 +0900

    余計なフッターSVGの無効化

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
       <a href="#_" class="text-xl font-bold leading-none text-gray-900 select-none logo">僕のボキ</a>
       <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2022 Developed by higakijin</p>
       <span class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
-        <a href="#" class="text-gray-400 hover:text-gray-500">
+        <!-- <a href="#" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">Facebook</span>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
             <path
@@ -33,7 +33,7 @@
               d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
             ></path>
           </svg>
-        </a>
+        </a> -->
 
         <a href="https://github.com/higakijin/boki" class="text-gray-400 hover:text-gray-500" target="_blank">
           <span class="sr-only">GitHub</span>
@@ -46,7 +46,7 @@
           </svg>
         </a>
 
-        <a href="#" class="text-gray-400 hover:text-gray-500">
+        <!-- <a href="#" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">Dribbble</span>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
             <path
@@ -55,7 +55,7 @@
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
+        </a> -->
       </span>
     </div>
   </section>
